a9d6f70e7db0cebd5fac1b45547da929059287fe
yoqubjonov <yoqubjonov98@mail.ru>
2024-01-15 08:55:24 +0500

    main carousel

--- components/Partials/MainCarousel.vue
+++ components/Partials/MainCarousel.vue
@@ -8,10 +8,10 @@ const slides = ref([
 </script>
 
 <template>
-  <div class="main-carousel-comp bg-black pb-3">
-    <v-carousel height="460" class="main-carousel" hide-delimiter-background cover> 
+  <div class="main-carousel-comp bg-black">
+    <v-carousel class="main-carousel" hide-delimiter-background cover> <!-- cycle -->
       <v-carousel-item v-for="(slide, i) in slides" :key="i">
-        <v-img :src="slide.image" />
+        <v-img :src="slide.image" max-width="89%" max-height="420" class="main-carousel-img my-7 mx-auto" cover />
       </v-carousel-item>
     </v-carousel>
   </div>
@@ -20,5 +20,14 @@ const slides = ref([
 <style lang="scss">
 .main-carousel-comp {
   background-color: var(--bg-black);
+  .main-carousel-img {
+    border-radius: 15px;
+    box-shadow: 
+      rgb(255 255 255 / 11%) 0px 24px 25px, 
+      rgb(255 255 255 / 12%) 0px -12px 30px, 
+      rgb(255 255 255 / 3%) 0px 4px 6px, 
+      rgb(255 255 255 / 5%) 0px 12px 13px, 
+      rgb(255 255 255 / 9%) 0px -3px 5px;
+  }
 }
 </style>
