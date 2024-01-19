f25f43187615388b4c82b5f5dafef91b1d35e9e8
Muhammad-Umar Yoqubjonov <Muhammad-Umar.Yoqubjonov>
2024-01-19 16:54:00 +0500

    singleProductDialog -> ready

--- components/Dialogs/SingleProduct.vue
+++ components/Dialogs/SingleProduct.vue
@@ -41,7 +41,7 @@ const setThumbsSwiper = (swiper: any) => {
 </script>
 
 <template>
-  <v-dialog v-model="viewDialog.dialog" width="1000">
+  <v-dialog v-model="viewDialog.dialog" class="single-product-dialog" width="1000">
     <v-card>
       <v-toolbar color="white" class="dialog-product-toolbar">
         <v-btn @click="$emit('emit:close')" icon>
@@ -50,8 +50,8 @@ const setThumbsSwiper = (swiper: any) => {
       </v-toolbar>
       <v-card-text class="pt-0">
         <v-row class="my-0">
-          <v-col cols="12" lg="6" class="pt-0">
-            <div class="quick-view-images">
+          <v-col cols="12" lg="6" md="6" class="pt-0">
+            <div class="quick-view-images mt-2">
               <div class="quick-gallery-images">
                 <swiper
                   :style="{
@@ -106,12 +106,12 @@ const setThumbsSwiper = (swiper: any) => {
             </div>
           </v-col>
 
-          <v-col cols="12" lg="6" class="pt-0">
+          <v-col cols="12" lg="6" md="6" class="pt-0">
             <div class="quick-view-description">
               <h4 class="product-name">Televizor VOLTO LED 65VUW-9000 Smart</h4>
               <div class="price d-flex align-center mt-2">
-                <div class="sale-price">4 999 000 сум</div>
-                <div class="old-price">5 999 000</div>
+                <div class="sale-price text-info">4 999 000 сум</div>
+                <!-- <div class="old-price">5 999 000</div> -->
               </div>
               <p class="mt-2">
                 Lorem ipsum dolor sit amet, consectet adipisicing elit, sed do
@@ -121,11 +121,11 @@ const setThumbsSwiper = (swiper: any) => {
                 dolor in reprehenderit in voluptate.
               </p>
               <div class="product-buttons">
+                <v-btn rounded="lg" height="55" class="mr-4">
+                  <v-img :src="'/_nuxt/assets/images/pay-systems/payme.svg'" width="100" />
+                </v-btn>
                 <v-btn rounded="lg" height="55">
-                  <v-img
-                    :src="'/_nuxt/assets/images/pay-systems/payme.svg'"
-                    width="100"
-                  />
+                  <v-img :src="'/_nuxt/assets/images/pay-systems/uzum.svg'" width="100" />
                 </v-btn>
               </div>
               <div class="product-info">
@@ -148,71 +148,10 @@ const setThumbsSwiper = (swiper: any) => {
   justify-content: flex-end;
 }
 
-.quick-view-images {
-  .quick-gallery-thumbs {
-    position: relative;
-  }
-}
-
-.quick-view-description {
-  .product-name {
-    font-size: 24px;
-    font-weight: 600;
-    color: #000;
-    margin-bottom: 0;
-  }
-
-  .price {
-    .sale-price {
-      font-size: 22px;
-      color: #00b8ff;
-      margin-top: 5px;
-      font-family: PoppinsMedium, sans-serif;
-    }
-
-    .old-price {
-      font-size: 18px;
-      font-family: PoppinsMedium, sans-serif;
-      color: #a4a4a4;
-      margin-top: 5px;
-      margin-left: 10px;
-      text-decoration: line-through;
-    }
-  }
-
-  p {
-    font-size: 15px;
-    line-height: 1.8;
-    color: #6c788c;
-    margin-bottom: 0;
-  }
-
-  .product-buttons {
-    display: flex;
-    flex-wrap: wrap;
-    padding-top: 25px;
-  }
-
-  .product-info {
-    padding-top: 25px;
-    .single-info {
-      display: flex;
-      align-items: center;
-      flex-wrap: wrap;
-      margin-top: 8px;
-      .label {
-        color: #3f3d3d;
-        font-size: 16px;
-        font-family: 'Rajdhani', sans-serif;
-        font-weight: 600;
-        margin-right: 5px;
-      }
-      .value {
-        color: #9f9e9e;
-        font-size: 16px;
-        font-family: 'Rajdhani', sans-serif;
-        font-weight: 600;
-      }
+@media (max-width: 960px) {
+  .single-product-dialog {
+    .v-overlay__content {
+      width: 600px !important;
     }
   }
 }
