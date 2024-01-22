033961a2c6396786bcb03d5ed6d5c9545122f564
Muhammad-Umar Yoqubjonov <Muhammad-Umar.Yoqubjonov>
2024-01-22 19:37:49 +0500

    adaptive settings

--- layouts/components/TheFooter.vue
+++ layouts/components/TheFooter.vue
@@ -8,7 +8,7 @@ const phoneNumber = ref('')
       <v-container>
         <div class="bottom_border">
           <v-row class="align-center">
-            <v-col cols="12" lg="6" md="6">
+            <v-col cols="6">
               <div class="query_text">
                 <h3>
                   У вас есть вопрос? <br />
@@ -16,7 +16,7 @@ const phoneNumber = ref('')
                 </h3>
               </div>
             </v-col>
-            <v-col cols="12" lg="6" md="6">
+            <v-col cols="6">
               <div class="query_number">
                 <h3>+998 99 999 88 88</h3>
                 <p><a href="#">support@gmail.com</a></p>
@@ -29,39 +29,41 @@ const phoneNumber = ref('')
     <div class="footer_top">
       <v-container>
         <v-row>
-          <v-col cols="12" xl="4" lg="4" md="6">
-            <div class="footer_widget">
-              <div class="footer_logo">
+          <v-col cols="12" xl="4" lg="4" md="4" sm="5">
+            <div class="footer_widget footer_middle_left">
+              <div class="footer_logo d-flex">
                 <a href="#">
                   <v-img :src="'/_nuxt/assets/images/volto.jpg'" width="100" />
                 </a>
               </div>
-              <p>
-                Sifatli va kafolatlangan mahsulotlar <br/>
-                O'zbekistonda ishlab chiqarilgan.
-              </p>
-              <div class="socail_links">
-                <ul>
-                  <li>
-                    <a href="#" class="flex-center">
-                      <v-icon color="white" class="ti-facebook">mdi-facebook</v-icon>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="flex-center">
-                      <v-icon color="white" class="fa fa-instagram">mdi-instagram</v-icon>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="flex-center">
-                      <v-img :src="'/_nuxt/assets/images/socials/tg.svg'" max-width="19" class="ti-twitter-alt" />
-                    </a>
-                  </li>
-                </ul>
+              <div class="footer_right_bottom">
+                <p>
+                  Sifatli va kafolatlangan mahsulotlar <br/>
+                  O'zbekistonda ishlab chiqarilgan.
+                </p>
+                <div class="socail_links">
+                  <ul>
+                    <li>
+                      <a href="#" target="_blank" class="flex-center">
+                        <v-icon color="white" class="ti-facebook">mdi-facebook</v-icon>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.instagram.com/volto.uzbekistan/?hl=en" target="_blank" class="flex-center">
+                        <v-icon color="white" class="fa fa-instagram">mdi-instagram</v-icon>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://t.me/VoltoUzbekistan" target="_blank" class="flex-center">
+                        <v-img :src="'/_nuxt/assets/images/socials/tg.svg'" max-width="19" class="ti-twitter-alt" />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </v-col>
-          <v-col cols="12" xl="4" lg="4" md="6">
+          <v-col cols="12" xl="4" lg="4" md="3" class="footer_useful_links">
             <div class="footer_widget">
               <h3 class="footer_title">Полезные ссылки</h3>
               <ul>
@@ -72,7 +74,7 @@ const phoneNumber = ref('')
               </ul>
             </div>
           </v-col>
-          <v-col cols="12" xl="4" lg="4" md="6">
+          <v-col cols="12" xl="4" lg="4" md="5" sm="7">
             <div class="footer_widget">
               <h3 class="footer_title">Связаться</h3>
               <form action="#" class="newsletter_form">
