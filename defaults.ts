5caa99a97bb103db5681dd9831a9c66e60edd599
yoqubjonov <yoqubjonov98@mail.ru>
2024-02-10 02:42:54 +0500

    default -> change

--- defaults.ts
+++ defaults.ts
@@ -1,16 +1,16 @@
 /*____________________ Server ____________________*/
-// export const URL_BASE = '/';
-// export const URL_IMG = '/files/';
+export const URL_BASE = '/';
+export const URL_IMG = '/files/';
 
 
 /*____________________ Test Local ____________________*/
-export const URL_BASE = 'http://100.67.211.148:5000/' // work
-export const URL_IMG = 'http://100.67.211.148:5000/images/' // work
+// export const URL_BASE = 'http://100.67.211.148:5000/' // work
+// export const URL_IMG = 'http://100.67.211.148:5000/images/' // work
 
 
 /*____________________ Test Global ____________________*/
-// export const URL_BASE = 'http://192.168.0.107:5000/' // home
-// export const URL_IMG = 'http://192.168.0.107:5000/images/' // home
+// export const URL_BASE = 'http://5.182.26.121:8081/' // server
+// export const URL_IMG = 'http://5.182.26.121:8081/images/' // server
 
 
 // export const LANG = navigator.language.substr(0, 2).toLowerCase() || 'ru';
