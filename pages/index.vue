97029e6d9bd07d5d94e746efeb02b4118c9720a9
Muhammad-Umar Yoqubjonov <Muhammad-Umar.Yoqubjonov>
2024-05-07 12:38:40 +0500

    Seo optimization

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,11 @@
     meta: [
       { name: 'author', content: 'Yokubjonov Mukhammad-Umar' },
       { name: 'description', content: 'VOLTO UZBEKISTAN mahsulotlarini arzon narxga xarid qilish. VOLTO UZBEKISTAN mahsulotlari bir sutkada bepul yetkazib berish bilan. Xaridorlar sharhlari. Aksiyalar va chegirmalar' },
-      { name: 'keywords', content: 'Volto tv, volto, tv, тв, телевизоры, Волто телевизоры, телевизоры Ташкент, VOLTO UZBEKISTAN' }
+      { name: 'keywords', content: 'Volto tv, volto, tv, тв, телевизоры, Волто телевизоры, телевизоры Ташкент, VOLTO UZBEKISTAN' },
+      { property: 'og:url', content: '' },
+      { property: 'og:title', content: '' },
+      { property: 'og:site_name', content: 'Volto' },
+      { property: 'og:description', content: '' },
     ]
   })
 </script>
